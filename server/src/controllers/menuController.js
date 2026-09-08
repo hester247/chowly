f93b8c175725eb08afacc1cd905709be905e0805
Esther Ebagua <Estherebaguaogiugo@gmail.com>
2026-09-08 14:49:02 +0100

    Fix menu query for deployed schema

--- server/src/controllers/menuController.js
+++ server/src/controllers/menuController.js
@@ -7,33 +7,19 @@ const getMenu = async (req, res) => {
     const result = await pool.query(
       `
       SELECT
-        mi.id,
-        mi.name,
-        mi.description,
-        mi.price,
-        mi.preparation_time,
-        mi.availability_status,
-
-        CASE
-          WHEN m.menu_type = 'food' THEN 'Food'
-          WHEN m.menu_type = 'drinks' THEN 'Drink'
-          ELSE 'Food and Drinks'
-        END AS item_type,
-
-        r.name AS restaurant_name
-
-      FROM menu_items mi
-
-      JOIN menus m
-        ON mi.menu_id = m.id
-
-      JOIN restaurants r
-        ON m.restaurant_id = r.id
-
-      WHERE m.restaurant_id = $1
-        AND mi.availability_status = 'available'
-
-      ORDER BY m.menu_type, mi.name;
+        id,
+        restaurant_id,
+        name,
+        item_type,
+        description,
+        price,
+        preparation_time,
+        availability_status,
+        created_at
+      FROM menu_items
+      WHERE restaurant_id = $1
+        AND availability_status = 'available'
+      ORDER BY item_type, name;
       `,
       [restaurantId]
     );
