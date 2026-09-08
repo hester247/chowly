f7bfc0b59537df1c7106f42ee5ce472d4adae115
Esther Ebagua <Estherebaguaogiugo@gmail.com>
2026-09-08 13:58:42 +0100

    Connect frontend to deployment API

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useMemo, useState } from "react";
 import "./App.css";
 
-const API_URL = "http://localhost:3001/api";
+const API_URL =
+  import.meta.env.VITE_API_URL ||
+  "http://localhost:3001/api";
 
 const imageMap = {
   FinFish:
