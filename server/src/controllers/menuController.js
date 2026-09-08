f662a5af72214958ea45705ff10ca34baf91ff89
Esther Ebagua <Estherebaguaogiugo@gmail.com>
2026-09-08 14:31:38 +0100

    Fix menu query for database schema

--- server/src/controllers/menuController.js
+++ server/src/controllers/menuController.js
@@ -10,17 +10,30 @@ const getMenu = async (req, res) => {
         mi.id,
         mi.name,
         mi.description,
-        mi.item_type,
         mi.price,
         mi.preparation_time,
         mi.availability_status,
+
+        CASE
+          WHEN m.menu_type = 'food' THEN 'Food'
+          WHEN m.menu_type = 'drinks' THEN 'Drink'
+          ELSE 'Food and Drinks'
+        END AS item_type,
+
         r.name AS restaurant_name
+
       FROM menu_items mi
+
+      JOIN menus m
+        ON mi.menu_id = m.id
+
       JOIN restaurants r
-        ON mi.restaurant_id = r.id
-      WHERE mi.restaurant_id = $1
+        ON m.restaurant_id = r.id
+
+      WHERE m.restaurant_id = $1
         AND mi.availability_status = 'available'
-      ORDER BY mi.item_type, mi.name;
+
+      ORDER BY m.menu_type, mi.name;
       `,
       [restaurantId]
     );
@@ -36,6 +49,7 @@ const getMenu = async (req, res) => {
     res.status(500).json({
       success: false,
       message: "Unable to fetch menu",
+      error: error.message,
     });
   }
 };
